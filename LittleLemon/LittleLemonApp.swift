4e0676b7f4be0f73d0066d3b49bc8d611b9ca1c1
John Raetzel <jdraetzel@gmail.com>
2025-11-17 07:50:05 -0500

    Initial Commit

---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by John Raetzel on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
